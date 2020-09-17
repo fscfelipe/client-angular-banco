f33bb1733597376c225d4cb6f536c2879a26ff7c
Felipe Sobreira <felipesobreira.c@hotmail.com>
2020-09-17 03:06:19 -0300

    Add: import de modulos, componentes e serviços

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { ListaContasComponent } from './lista-contas/lista-contas.component';
+
+import { ContaService } from './conta.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaContasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ContaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
